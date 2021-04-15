6777917ed8741b7a3ddd2d032ff5f77c4bf773af
Amanda <amanda.garrison.woods@gmail.com>
2021-04-15 11:18:28 -0500

    completed operators exercise

---
+++ js/operators.js
@@ -0,0 +1,33 @@
+'1' + 2
+    "12"
+typeof '1' + 2
+    string
+6 % 4
+    2
+'3 + 4 is ' + 3 + 4
+    "3 + 4 is 34"
+    //to get correct answer '3 + 4 is ' + "3 + 4"
+0 < 0
+    false
+'false' == false
+    false
+true == 'true'
+    false
+5 >= -5
+    true
+!true && !false
+    false
+6 % 5
+    1
+5 < 4 && 1 === 1
+    false
+typeof 'codeup' === 'string'
+    true
+'codeup' === 'codeup' && 'codeup' === 'Codeup'
+    false
+4 >= 0 && 1 !== '1'
+    true
+6 % 3 === 0
+    false
+5 % 2 !== 0
+    true
